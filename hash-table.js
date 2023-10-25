4f3d0cb009b8ae468fcb12bb16eee4a70f72b5c6
charlesoller <charlesrello@gmail.com>
2023-10-25 18:41:42 -0400

    Complete

--- hash-table.js
+++ hash-table.js
@@ -11,29 +11,71 @@ class KeyValuePair {
 class HashTable {
 
   constructor(numBuckets = 4) {
-    // Your code here 
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity)
+    this.data.fill(null)
   }
 
   hash(key) {
-    // Your code here 
+    let hashed = sha256(key);
+    return parseInt(`0x${hashed.slice(0, 8)}`);
   }
 
   hashMod(key) {
-    // Your code here 
+    //This is used to generate the index for this.data
+    return this.hash(key) % this.capacity;
   }
 
   insertNoCollisions(key, value) {
-    // Your code here 
+    let index = this.hashMod(key);
+    if(!this.data[index]){
+      this.data[index] = new KeyValuePair(key, value);
+      this.count++;
+    } else {
+      throw new Error("hash collision or same key/value pair already exists!");
+    }
   }
 
   insertWithHashCollisions(key, value) {
-    // Your code here 
+    let index = this.hashMod(key);
+    if(!this.data[index]){
+      this.data[index] = new KeyValuePair(key, value);
+      this.count++;
+    } else {
+      let prevPair = this.data[index];
+      this.data[index] = new KeyValuePair(key, value);
+      this.data[index].next = prevPair;
+      this.count++;
+    }
   }
 
   insert(key, value) {
-    // Your code here 
-  }
+    let index = this.hashMod(key);
+    if(!this.data[index]){
+      //This index is empty
+      this.data[index] = new KeyValuePair(key, value);
+      this.count++;
+    } else {
+      //This index is already filled
+      //loops from beginning of LL to check if the key already exists at the occupied index
+      let curr = this.data[index];
+      while(curr){
+        if(curr.key === key){
+          curr.value = value;
+          return;
+        }
+        curr = curr.next
+      }
 
+      //Index is filled, but the key does not yet exist
+      //inserts at head
+      let prevPair = this.data[index];
+      this.data[index] = new KeyValuePair(key, value);
+      this.data[index].next = prevPair;
+      this.count++;
+      }
+    }
 }
 
 
